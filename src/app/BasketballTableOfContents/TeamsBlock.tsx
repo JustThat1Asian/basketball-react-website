7090fc80a3586ced9bdc8d58ab764aa9d9e9e218
JustThat1Asian <104869846+JustThat1Asian@users.noreply.github.com>
2024-11-24 21:28:05 -0500

    refactored CSS

--- src/app/BasketballTableOfContents/TeamsBlock.tsx
+++ src/app/BasketballTableOfContents/TeamsBlock.tsx
@@ -11,14 +11,11 @@ interface TeamsBlockProp {
 
 const TeamsBlock = ({ name, id, teamCode, nickName }: TeamsBlockProp) => {
   return (
-    <tr
-      key={id}
-      className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
-    >
-      <td className="px-6 py-4">{id}</td>
-      <td className="px-6 py-4">{name}</td>
-      <td className="px-6 py-4">{nickName}</td>
-      <td className="px-6 py-4">{teamCode}</td>
+    <tr key={id}>
+      <td>{id}</td>
+      <td>{name}</td>
+      <td>{nickName}</td>
+      <td>{teamCode}</td>
     </tr>
   );
 };
--- src/app/TeamDetails/page.tsx
+++ src/app/TeamDetails/page.tsx
@@ -20,8 +20,8 @@ const Page = async ({ teamId, teamName }: TeamProp) => {
   return (
     <>
       <h1>{teamName} Page</h1>
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table>
+        <thead>
           <tr>
             <th>Player Name</th>
             <th>Age</th>
